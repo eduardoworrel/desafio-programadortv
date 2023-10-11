3cbc14e72acc49b0e0c7b07b7df43a6deccfd19c
eduardo.worrel <eduardo.worrel@gmail.com>
2023-10-11 20:53:04 -0300

    dia 2

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -19,21 +21,56 @@ public class IndexModel : PageModel
     {
 
     }
-    public IActionResult OnPost()
+    public async Task<IActionResult> OnPostAsync()
     {
 
         if (Files == null || Files.Length == 0)
             return BadRequest("file not selected");
+
+
+
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "ffprobe",
+                Arguments = "-v error -hide_banner -print_format json -show_format -show_streams pipe:0",
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.Start();
+
+        Task.Run(() =>
+        {
+           while(!process.StandardError.EndOfStream)
+           {
+               var line = process.StandardError.ReadLine();
+               _logger.LogInformation(line);
+           }
+        });
+
+        await Files.CopyToAsync(process.StandardInput.BaseStream);
+
+        process.StandardInput.Close();
+
+        process.WaitForExit();
+        
+        var json = process.StandardOutput.ReadToEnd();
         
 
         var result = new {
             FileName = Path.GetFileNameWithoutExtension(Files.FileName),
             Extension = Path.GetExtension(Files.FileName),
-            FileSize = (Files.Length / 1024) + " KB",
+            FileSize = (Files.Length / (1024 * 1024)) + " MB",
             ContentType = Files.ContentType,
+            ffmpegData = json
         };
-
-        
+  
         return new JsonResult(result);
     }
 }
